10d85f03ab6b60e9939f391071c086fa1b974452
Matthew Segal <mattdsegal@gmail.com>
2021-03-23 09:14:44 +1100

    isort

--- tests/test_stochastic/test_stochastic_flows.py
+++ tests/test_stochastic/test_stochastic_flows.py
@@ -1,7 +1,7 @@
-import pytest
-from scipy.stats import binom
 import numpy as np
+import pytest
 from numpy.testing import assert_allclose, assert_array_equal
+from scipy.stats import binom
 
 from summer import CompartmentalModel
 
