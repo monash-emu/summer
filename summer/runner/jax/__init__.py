5125f2628064c63f270113476bada7f7b2638863
David Shipman <dshipman@gmail.com>
2022-07-13 06:30:54 +1000

    Set CPU as default jax target (silence warnings)

--- summer/runner/jax/__init__.py
+++ summer/runner/jax/__init__.py
@@ -7,5 +7,6 @@ from jax.config import config as _jax_config
 
 # Jax configuration
 # FIXME: We need to find a more appropriate place to ensure this happens globally
+_jax_config.update("jax_platform_name", "cpu")
 _jax_config.update("jax_enable_x64", True)
 _os.environ["XLA_FLAGS"] = "--xla_cpu_multi_thread_eigen=false intra_op_parallelism_threads=1"
