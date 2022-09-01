32e1486471a899328f337a5d0a5077738f910b66
David Shipman <dshipman@gmail.com>
2022-09-02 06:20:55 +1000

    Revert buggy compartment validation

--- summer/stratification.py
+++ summer/stratification.py
@@ -301,10 +301,6 @@ class Stratification:
         Returns the new compartments.
         """
 
-        for c in self.compartments:
-            if c not in comps:
-                raise Exception("Trying to stratify non-existent compartment", c)
-
         new_comps = []
         for old_comp in comps:
             should_stratify = old_comp.has_name_in_list(self.compartments)
